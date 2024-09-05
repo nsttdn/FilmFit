41e7b144bcd46f47d778aba62069b567a06a9464
nsttdn <dejnekan@gmail.com>
2024-09-06 00:54:39 +0300

    start autentification

--- app/src/main/java/com/example/filmfit/MainActivity.kt
+++ app/src/main/java/com/example/filmfit/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.filmfit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
